e76d0ff584ef01778fb3d7d4d90e643529369c99
shivanshu-pattnaik <74895692+ShivanshuPattnaik@users.noreply.github.com>
2020-12-23 11:51:35 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# GCSO-TEMPORARY
-This is a Temporary Project
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,58 @@
+// GCSO
+// BY SHIVANSHU PATTNAIK
+
+// VARIABLES
+var car;
+var wall; 
+
+var speed;
+var weight;
+
+function setup() {
+  
+  createCanvas(1600, 400);
+
+  weight = random(400, 1500);
+  speed = random(55, 90);
+
+  car = createSprite(50, 200, 50, 50);
+  car.velocityX = speed;
+
+  wall = createSprite(1500, 200, 60, height / 2);
+  wall.shapeColor = "white";
+
+}
+
+function draw() {
+  
+  background("black");
+
+  if(wall.x - car.x < (car.width + wall.width) / 2){
+
+    car.velocityX = 0;
+
+    var deformation = 0.5 * weight * speed * speed / 22500;
+
+    if(deformation > 180){
+
+      car.shapeColor = "red";
+
+    }
+    
+    if(deformation > 100 && deformation < 180){
+
+      car.shapeColor = "yellow";
+
+    }
+    
+    if(deformation < 100){
+
+      car.shapeColor = "green";
+
+    }
+
+  }
+
+  drawSprites();
+
+}
